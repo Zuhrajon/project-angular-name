d36918b32ffdb446ba698d9b4ac4d9d99f62ae8a
Daler <isoev9733@gmail.com>
2025-04-18 18:08:50 +0400

    Правка svg компонента

--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'svg[icon]',
   standalone: true,
   imports: [],
-  template: '<svg:use [attr.href]="href"></svg:use>',
+  template: ' <svg class="icon" xmlns="http://www.w3.org/2000/svg">\n' +
+    '      <use [attr.href]="href" [attr.xlink:href]="href"></use>\n' +
+    '    </svg>',
   styles: ['']
 })
 export class SvgIconComponent {
